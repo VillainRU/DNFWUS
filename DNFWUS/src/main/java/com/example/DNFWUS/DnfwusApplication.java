37970c64d0de60d3109bb9de40821aa89a59e943
VillainRU <electro.dmitriy@gmail.com>
2025-10-27 19:19:25 +0400

    1

---
+++ DNFWUS/src/main/java/com/example/DNFWUS/DnfwusApplication.java
@@ -0,0 +1,13 @@
+package com.example.DNFWUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfwusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnfwusApplication.class, args);
+	}
+
+}
---
+++ DNFWUS/src/test/java/com/example/DNFWUS/DnfwusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DNFWUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnfwusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
